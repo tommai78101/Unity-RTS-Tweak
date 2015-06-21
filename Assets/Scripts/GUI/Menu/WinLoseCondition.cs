a72ea00f010d702c1084945dbdc080b1c53114e4
Asperatology <tom.mai78101@gmail.com>
2015-06-21 16:46:43 -0400

    Added winning/losing condtions.
    It's now a fully-fledged game prototype.
    
    (Weak prototype though)

---
+++ Assets/Scripts/GUI/Menu/WinLoseCondition.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WinLoseCondition : MonoBehaviour {
+	public NetworkView playerNetworkView;
+	public bool playerHasLost;
+	public static List<NetworkPlayer> Losers;
+
+	public delegate void DG_PlayerLost();
+	public event DG_PlayerLost EVENT_PlayerHasLost;
+
+	private void Start() {
+		this.playerNetworkView = this.gameObject.GetComponent<NetworkView>();
+		if (this.playerNetworkView == null) {
+			Debug.LogError(new System.NullReferenceException("WinLoseCondition: Unable to find network view."));
+		}
+		this.playerHasLost = false;
+		Debug.Log("WinLoseCondition: Adding SetPlayerLost() to event delegate.");
+		this.EVENT_PlayerHasLost += new WinLoseCondition.DG_PlayerLost(SetPlayerLost);
+	}
+
+	private void OnGUI() {
+		if (this.playerHasLost) {
+			GUI.Label(new Rect((Screen.width - 50) / 2f, (Screen.height - 15) / 2f, 100f, 30f), "YOU LOSE!");
+		}
+	}
+
+	//[RPC]
+	//public void RPC_NotifyWinLose(NetworkViewID loserID) {
+	//	NetworkView viewID = NetworkView.Find(loserID);
+	//	WinLoseCondition.Losers.Add(viewID.owner);
+	//	WinLoseCondition condition = viewID.gameObject.GetComponent<WinLoseCondition>();
+	//	if (condition != null) {
+	//		condition.playerHasLost = true;
+	//	}
+	//}
+
+	public void PlayerHasLost() {
+		Debug.Log("WinLoseCondition: The player has just lost. Calling PlayerHasLost().");
+		//if (UnitManager.Instance.PlayerUnits.Count <= 0 && !this.playerHasLost && Network.connections.Length > 0) {
+		//	this.playerNetworkView.RPC("RPC_NotifyWinLose", RPCMode.AllBuffered, this.playerNetworkView.viewID);
+		//}
+		this.EVENT_PlayerHasLost();
+	}
+
+	public void SetPlayerLost() {
+		Debug.Log("WinLoseCondition: Calling SetPlayerLost().");
+		this.playerHasLost = true;
+	}
+
+}
--- Assets/Scripts/Unit/Perishable.cs
+++ Assets/Scripts/Unit/Perishable.cs
@@ -8,6 +8,14 @@ public class Perishable : MonoBehaviour {
 			UnitManager.Instance.PlayerUnits.Remove(this.gameObject);
 			UnitManager.Instance.AllUnits.Remove(this.gameObject);
 			Debug.Log("Current unit count: " + UnitManager.Instance.PlayerUnits.Count.ToString());
+
+			if (UnitManager.Instance.PlayerUnits.Count <= 0) {
+				WinLoseCondition condition = UnitManager.Instance.gameObject.GetComponent<WinLoseCondition>();
+				if (condition != null) {
+					Debug.Log("WinLoseCondition found! Calling on event and delegate triggers.");
+					condition.PlayerHasLost();
+				}
+			}
 		}
 	}
 }
