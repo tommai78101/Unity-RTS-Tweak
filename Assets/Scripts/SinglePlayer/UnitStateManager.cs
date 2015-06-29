e4f6ae7c4e5e104227030c16aa1f1896b8c1aaf9
Asperatology <tom.mai78101@gmail.com>
2015-06-28 20:29:36 -0400

    Tweaked moving so that it can be re-ordered to move to a different destination while moving.

--- Assets/Scripts/SinglePlayer/UnitStateManager.cs
+++ Assets/Scripts/SinglePlayer/UnitStateManager.cs
@@ -40,33 +40,32 @@ public class UnitStateManager : MonoBehaviour {
 	}
 
 	private void Update() {
-		if (this.selectFlag && Input.GetMouseButtonUp(1) && this.actionState.Equals(UnitCommand.NO_ORDERS)) {
-			this.actionState = UnitState.WAITING_FOR_ORDERS;
-			return;
-		}
-		switch (this.actionState) {
-			default:
-			case UnitState.WAITING_FOR_ORDERS:
-				WaitingOnOrders();
-				break;
-			case UnitState.ATTACK:
-				AttackAction();
-				break;
-			case UnitState.DIVIDING:
-				DivideAction();
-				break;
-			case UnitState.DYING:
-				DeathAction();
-				break;
-			case UnitState.IDLE:
-				IdleAction();
-				break;
-			case UnitState.MERGING:
-				MergeAction();
-				break;
-			case UnitState.SCOUTING:  //Moving
-				ScoutAction();
-				break;
+		if (this.selectFlag) {
+			switch (this.actionState) {
+				default:
+				case UnitState.WAITING_FOR_ORDERS:
+					WaitingOnOrders();
+					break;
+				case UnitState.ATTACK:
+					AttackAction();
+					break;
+				case UnitState.DIVIDING:
+					DivideAction();
+					break;
+				case UnitState.DYING:
+					DeathAction();
+					break;
+				case UnitState.IDLE:
+					IdleAction();
+					break;
+				case UnitState.MERGING:
+					MergeAction();
+					break;
+				case UnitState.SCOUTING:  //Moving
+					ScoutAction();
+					break;
+			}
+			HandleOrders();
 		}
 	}
 
@@ -77,7 +76,8 @@ public class UnitStateManager : MonoBehaviour {
 					if (this.agent.hasPath || this.agent.velocity.sqrMagnitude == 0f) {
 						//Stopped.
 						float distance = Vector3.Distance(this.transform.position, this.attackee.transform.position);
-						if (distance >= 2.3f) {
+						if (distance >= 1f) {
+							this.agent.stoppingDistance = 0.9f;
 							this.agent.SetDestination(this.attackee.transform.position);
 						}
 					}
@@ -115,7 +115,7 @@ public class UnitStateManager : MonoBehaviour {
 	}
 
 	private void ScoutAction() {
-
+		ReachingDestination();
 	}
 
 	private void WaitingOnOrders() {
@@ -134,6 +134,7 @@ public class UnitStateManager : MonoBehaviour {
 			}
 			else if (Input.GetMouseButtonUp(1)) {
 				this.commandState = UnitCommand.MOVE_ORDER;
+				Move();
 				Debug.Log("MOVE ORDER");
 			}
 			else if (Input.GetMouseButtonUp(0)) {
@@ -148,13 +149,12 @@ public class UnitStateManager : MonoBehaviour {
 		//Check if enemies are nearby.
 		bool currentState = this.actionState.Equals(UnitState.IDLE) || this.actionState.Equals(UnitState.WAITING_FOR_ORDERS) || this.actionState.Equals(UnitState.ATTACK);
 		if (currentState) {
-			Collider[] colliders = Physics.OverlapSphere(this.transform.position, 1f);
+			Collider[] colliders = Physics.OverlapSphere(this.transform.position, 3f);
 			bool enemyNearby = false;
-			for (int i = 0; i <colliders.Length; i++){
-				if (!colliders[i].name.Equals("Floor") && !colliders[i].gameObject.Equals(this.gameObject)){
+			for (int i = 0; i < colliders.Length; i++) {
+				if (!colliders[i].name.Equals("Floor") && !colliders[i].gameObject.Equals(this.gameObject)) {
 					float distance = Vector3.Distance(colliders[i].transform.position, this.transform.position);
-					Debug.Log("Distance: " + distance);
-					if (distance < 1f) {
+					if (distance < 4f) {
 						this.attackee = colliders[i].gameObject;
 						this.SendMessage("AttackObject", this.attackee);
 						enemyNearby = true;
@@ -164,6 +164,47 @@ public class UnitStateManager : MonoBehaviour {
 			}
 			if (this.attackee != null && !enemyNearby) {
 				this.attackee = null;
+			}
+		}
+	}
+
+	private void HandleOrders() {
+		switch (this.commandState) {
+			case UnitCommand.NO_ORDERS:
+				if (Input.GetMouseButtonUp(1)) {
+					Move();
+				}
+				break;
+			case UnitCommand.MOVE_ORDER:
+				if (Input.GetMouseButtonUp(1)) {
+					Move();
+				}
+				//CheckSurroundings();
+				break;
+			default:
+				break;
+		}
+	}
+
+	private void Move() {
+		Debug.Log("Giving attack order and setting new destination.");
+		this.actionState = UnitState.SCOUTING;
+		this.commandState = UnitCommand.MOVE_ORDER;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hitInfo;
+		if (Physics.Raycast(ray, out hitInfo)) {
+			Debug.Log("New move target position: " + hitInfo.point);
+			this.agent.SetDestination(hitInfo.point);
+		}
+	}
+
+	private void ReachingDestination() {
+		if (!this.agent.pathPending) {
+			if (this.agent.remainingDistance <= this.agent.stoppingDistance) {
+				if (this.agent.hasPath || this.agent.velocity.sqrMagnitude == 0f) {
+					this.actionState = UnitState.IDLE;
+					this.commandState = UnitCommand.NO_ORDERS;
+				}
 			}
 		}
 	}
