db48206635e136287401ac4cee6d3cf67880f0ed
Asperatology <tom.mai78101@gmail.com>
2015-06-29 16:34:40 -0400

    Forgot to add this in.

--- Assets/Scripts/SinglePlayer/UnitHealth.cs
+++ Assets/Scripts/SinglePlayer/UnitHealth.cs
@@ -8,6 +8,7 @@ public class UnitHealth : MonoBehaviour {
 	private float redCountdown;
 	private Color initialColor;
 	private Material material;
+	private bool isTakingDamage = false;
 
 	private void Start() {
 		this.healthPoints = this.initialHealthPoints;
@@ -17,6 +18,7 @@ public class UnitHealth : MonoBehaviour {
 			this.material = renderer.material;
 		}
 		this.redCountdown = 0f;
+		this.isTakingDamage = false;
 	}
 
 	public void TakeDamage() {
@@ -32,19 +34,22 @@ public class UnitHealth : MonoBehaviour {
 	}
 
 	private void Update() {
-		if (this.material != null) {
+		if (this.material != null && this.isTakingDamage) {
 			if (this.redCountdown > 0f) {
-				this.material.color = Color.Lerp(this.initialColor, Color.red, this.redCountdown);
+				this.redCountdown -= Time.deltaTime;
+				this.material.color = Color.Lerp(Color.red, this.initialColor, 1f - this.redCountdown);
 			}
 			else {
-				this.redCountdown -= Time.deltaTime / 3f;
+				this.material.color = this.initialColor;
+				this.isTakingDamage = false;
 			}
 		}
 	}
 
 	private void FlashRed() {
-		if (this.redCountdown <= 0f) {
+		if (this.material != null) {
 			this.redCountdown = 1f;
+			this.isTakingDamage = true;
 		}
 	}
 }
