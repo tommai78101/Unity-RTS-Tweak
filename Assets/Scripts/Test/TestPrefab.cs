97aa089f6dce1dc0c2e113fa4cc359b52d2fe820
Asperatology <tom.mai78101@gmail.com>
2015-09-02 13:52:42 -0400

    Adding test branch, done to learn more about networking.

---
+++ Assets/Scripts/Test/TestPrefab.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class TestPrefab : NetworkBehaviour {
+	void Update () {
+		if (this.isServer) {
+			ServerTest();
+		}
+		else {
+			ClientTest();
+		}
+	}
+
+	public void Call() {
+		Debug.Log("Call");
+	}
+
+	[Command]
+	public void CmdCall() {
+		Debug.Log("CMD Call");
+	}
+
+	[ClientRpc]
+	public void RpcCall() {
+		Debug.Log("RPC Call");
+		CmdCall();
+	}
+
+	public void ServerTest() {
+		RpcCall();
+	}
+
+	public void ClientTest() {
+		Call();
+	}
+}
