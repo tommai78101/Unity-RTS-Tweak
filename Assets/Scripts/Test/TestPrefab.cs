ce396429f3010016713788ad0730ec5fb7f7eebb
Asperatology <tom.mai78101@gmail.com>
2015-09-02 16:55:41 -0400

    Ugh

--- Assets/Scripts/Test/TestPrefab.cs
+++ Assets/Scripts/Test/TestPrefab.cs
@@ -6,7 +6,7 @@ public class TestPrefab : NetworkBehaviour {
 	public GameObject something;
 
 	void Update() {
-		if (Input.GetMouseButton(0)) {
+		if (this.isLocalPlayer && Input.GetMouseButton(0)) {
 			if (this.isServer) {
 				RpcCall();
 			}
